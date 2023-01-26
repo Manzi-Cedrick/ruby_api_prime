7a25187ff8656dd3209bb692e9315799adf67622
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-01-26 09:46:18 +0200

    [add] renovated routes functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :bands
+  # devise_for :users
+  namespace :v1 do 
+    resources :contacts
+    resources :sessions , only: [:create,:destroy]
+  end 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
