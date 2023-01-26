3735b33c4572b4f98ba0c1092807ae693616d367
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-01-26 09:45:44 +0200

    [add] routes functionality

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,15 @@ module PrimeApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.middleware.insert_before 0, Rack::Cors do 
+      allow do 
+        origins '*'
+        resource(
+          '*',
+          headers: :any, 
+          methods: [:get,:put,:delete,:patch,:post,:options]
+        )
+      end
+    end
   end
 end
